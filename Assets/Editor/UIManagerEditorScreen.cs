9da82f39527f131133534fa36f6f48936ab68594
Wenguang <wenguang@playbiba.com>
2015-05-04 17:02:41 -0700

    editor screen

--- Assets/Editor/UIManagerEditorScreen.cs
+++ Assets/Editor/UIManagerEditorScreen.cs
@@ -4,6 +4,9 @@ using UnityEditor;
 
 public class UIManagerEditorScreen : EditorWindow {
 
+	public static bool autoLoad=false;
+	public Object bootSceneObj;
+
 	// Add menu named "Setting" to the UIManager menu
 	[MenuItem ("UIManager/Setting")] 
 	static void Init () {
@@ -15,5 +18,16 @@ public class UIManagerEditorScreen : EditorWindow {
 		//create autoload checkbox here
 		//maybe add all the screen list to here
 		//reference the boot scene in here
+
+		GUILayout.Label ("UIManager Editor Settings",EditorStyles.boldLabel);
+		EditorGUILayout.HelpBox ("Drag the Boot scene of your game into the object field and if the Auto-load toggle is checked, every time you play the game it will start from your boot scene",MessageType.Info); 
+		autoLoad = EditorGUILayout.Toggle ("Auto-load boot scene",autoLoad);
+		EditorGUILayout.BeginHorizontal ();
+		GUILayout.Label ("Boot Scene");
+		bootSceneObj = EditorGUILayout.ObjectField (bootSceneObj,typeof(Object),true);
+		EditorGUILayout.EndHorizontal ();
+
+		//help link
+	http://docs.unity3d.com/ScriptReference/EditorGUILayout.ObjectField.html
 	}
 }
